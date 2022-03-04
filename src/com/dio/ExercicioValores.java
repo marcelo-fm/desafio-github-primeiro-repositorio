cfabb313161788c08115252262ced3f17aec9054
marcelo-fm <foschiera.marcelom@gmail.com>
2022-03-04 10:40:11 -0300

    Update ExercicioValores.java

--- src/com/dio/ExercicioValores.java
+++ src/com/dio/ExercicioValores.java
@@ -1,5 +1,30 @@
 package com.dio;
 
+import javax.print.event.PrintEvent;
+
 public class ExercicioValores {
-    
+    public static void main(String[] args) {
+        
+        int i;
+        int I;
+        //int i;
+        int a;
+        int b;
+        int c;
+
+        i = 5;
+        I = 2;
+        a = 8;
+        b = 12;
+        c = 7;
+
+        final int J = 10;
+
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("c: " + c);
+        System.out.println("i: " + i);
+        System.out.println("I: " + I);
+        System.out.println("J: " + J);
+    }
 }
