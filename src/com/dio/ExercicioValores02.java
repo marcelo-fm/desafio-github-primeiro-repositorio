440fe832a553128de88259b5354a77d5241201f7
marcelo-fm <foschiera.marcelom@gmail.com>
2022-03-09 17:47:29 -0300

    Create ExercicioValores02.java

---
+++ src/com/dio/ExercicioValores02.java
@@ -0,0 +1,53 @@
+package com.dio;
+
+public class ExercicioValores02 {
+    public static void main(String[] args) {
+        
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        //short s2 = 40000;
+
+        int i1 = 28500;
+        //int i2 = -100000000000000;
+
+        long l1 = 1000000000000000000L;
+        //long l2 = 43248292198309126L;
+
+        float f1 = 10.68F;
+        //float f2 = 4.5;
+
+        double d1 = 99.84d;
+        //double d2 = 85.69;
+
+        char c1 = 'W';
+        //char c2 = 'Tw';
+        char c3 = '\u0057';
+
+        String st1 = "Fulano";
+        String st2 = "Cicrano";
+        String st3 = "asd jkdlas % &&!@ blablabla";
+
+        String dt1 = "09/02/1981";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i1);
+        System.out.println(l1);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(dt1);
+        System.out.println(bo1);
+        System.out.println(bo2);
+    }
+}
