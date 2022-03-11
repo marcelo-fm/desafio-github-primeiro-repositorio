ea910e22b916aba4eff4de10890898872f146a2b
marcelo-fm <foschiera.marcelom@gmail.com>
2022-03-11 20:23:56 -0300

    Update ExercicioOperadores.java

--- src/com/dio/ExercicioOperadores.java
+++ src/com/dio/ExercicioOperadores.java
@@ -13,7 +13,7 @@ public class ExercicioOperadores {
         atribuicao();
         System.out.println("");
         System.out.println("Precedência");
-
+        precedencia();
         
     }
 
@@ -64,5 +64,48 @@ public class ExercicioOperadores {
 
         System.out.println("d: " + d);
 
+        i += 5; // i = i + 5
+        j -= 3; // j = j - 3
+        d /= 2.7D; // d = d / 2.7D
+        l *= 3; // l = l * 3
+        k %= 2; // k = k % 2
+
+        System.out.println("i: " + i);
+        System.out.println("j: " + j);
+        System.out.println("d: " + d);
+        System.out.println("l: " + l);
+        System.out.println("k: " + k);
+
+
+        i = k = j;
+
+        System.out.println("k: " + k);
+        System.out.println("i: " + i);
+
+    }
+
+    private static void precedencia() {
+
+        int i = 10;
+        int j = 20;
+        int k = 30;
+
+        int a = i++ + --j * k; // 10 + 19 * 30 -> 10 + 570 -> 580
+
+        System.out.println("i++ + --j * k: " + a);
+
+        System.out.println("i: " + i); // 11
+
+        int b = k / --i % 3 + 1; // 30 / 10 % 3 + 1 -> 1
+
+        System.out.println("k / --i % 3 + 1: " + b);
+
+        System.out.println("i: " + i); // 10
+
+        int c = 2;
+
+        c *= i += 5; // c = 2 * 1; 1 = 1 + 5 -> c = 2 * 1; i = 10 + 5 -> c = 2 * 15; -> c = 2 * 15; c = 30
+
+        System.out.println("c *= i += 5: " + c);
     }
 }
